f42ae0ecc3a7b8d768baa9d1d09ce1f4a4383391
gabrielvictordm <69441049+gabrielvictordm@users.noreply.github.com>
2023-11-16 00:03:00 -0300

    Add files via upload

---
+++ Cliente.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using MySql.Data.MySqlClient;
+
+public class Program
+{
+    private const int ServerPort = 8080;
+
+    static bool IsValidPacket(string packetData)
+    {
+        try
+        {
+            using (var conn = new MySqlConnection("server=localhost;Database=devstatus;port=3306;User=root;Password=123456;"))
+            {
+                conn.Open();
+                using (var cmd = new MySqlCommand("SELECT validate_packet(@packetData)", conn))
+                {
+                    cmd.Parameters.AddWithValue("@packetData", packetData);
+                    var result = (bool)cmd.ExecuteScalar();
+                    return result;
+                }
+            }
+        }
+        catch (MySqlException e)
+        {
+            Console.WriteLine($"Erro ao conectar ou executar a consulta: {e.Message}");
+            return false;
+        }
+    }
+
+    static void SendPacket(string packet)
+    {
+        using (var udpClient = new UdpClient())
+        {
+            var serverEndpoint = new IPEndPoint(IPAddress.Loopback, ServerPort);
+            var messageBytes = System.Text.Encoding.UTF8.GetBytes(packet);
+            udpClient.Send(messageBytes, messageBytes.Length, serverEndpoint);
+            Console.WriteLine($"Pacote enviado para o servidor: {packet}");
+        }
+    }
+
+    
+
+    static string GenerateRandomPacket()
+    {
+        Random random = new Random();
+
+        int type = random.Next(1, 3); // Gera um número aleatório entre 1 e 2
+        int protocolo = random.Next(66, 69); // Gera um número aleatório entre 66 e 68
+        DateTime data = DateTime.Now;
+        int status = random.Next(2); // Gera um número aleatório entre 0 e 1
+        string id = GenerateRandomId();
+
+        return $">DATA{type},{protocolo},{data:yyMMddHHmmss},{status};ID={id}<";
+    }
+
+    static string GenerateRandomId()
+    {
+        const string characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        char[] idArray = new char[3];
+        Random random = new Random();
+
+        for (int i = 0; i < 3; i++)
+        {
+            idArray[i] = characters[random.Next(characters.Length)];//to pegando 1 caracter entre a posição 0 e a 63 aonde 0 é o A e o 63 seria o 9
+        }
+
+        return new string(idArray);
+    }
+
+    static void Main()
+    {
+        try
+        {
+            // Loop infinito para enviar pacotes aleatórios a cada 5 segundos
+                while (true)
+                {
+                    string randomPacket = GenerateRandomPacket();
+                    SendPacket(randomPacket);
+
+                    // Aguarda 5 segundos antes de enviar o próximo conjunto de dados
+                    System.Threading.Thread.Sleep(5000);
+        
+                }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Erro: {ex.Message}");
+        }
+    }
+}
---
+++ Servidor.cs
@@ -0,0 +1,222 @@
+﻿// Importações de namespaces necessários
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Text.Json.Serialization;
+using System.Security.Cryptography.X509Certificates;
+using MySql.Data.MySqlClient;
+using MySql.Data;
+using System.Data.SQLite;
+using System.Text.Json;
+using System.Data.SqlClient;
+// Definição da classe principal do servidor
+public class SocketServer
+{
+    // Constantes para configurações do servidor
+    private const int PORT = 8080;
+    private const int MAX_CLIENTS = 20;
+
+    // Lista para armazenar sockets dos clientes
+    private static List<Socket> clients = new List<Socket>();
+
+    // Flag para controle do loop principal do servidor
+    private static bool running = true;
+
+    // Conexão com o banco de dados SQLite
+    private static SQLiteConnection dbConnection;
+
+    // Cliente UDP para receber dados
+    private static UdpClient udpServer;
+
+    // Método principal que inicia o servidor
+    public static void Main()
+    {
+        // Inicializa o banco de dados SQLite
+        InitializeDatabase();
+
+        // Converte o endereço IP para um formato adequado
+        IPAddress ip = IPAddress.Parse("127.0.0.1");
+
+        // Inicializa o cliente UDP para receber dados na porta especificada
+        UdpClient udpServer = new UdpClient(PORT);
+
+        // Mensagem indicando que o servidor está rodando
+        Console.WriteLine("Servidor conectado...");
+
+        // Loop principal do servidor
+        while (running)
+        {
+            // Aguarda a recepção de dados
+            IPEndPoint clientEndpoint = new IPEndPoint(IPAddress.Any, 0);
+            byte[] buffer = udpServer.Receive(ref clientEndpoint);
+
+            // Adiciona o socket do cliente à lista, se houver espaço
+            if (clients.Count < MAX_CLIENTS)
+            {
+                clients.Add(udpServer.Client);
+                Console.WriteLine("Cliente conectado: " + clientEndpoint);
+
+                // Inicia uma nova thread para lidar com os dados do cliente
+                Thread clientThread = new Thread(new ParameterizedThreadStart(HandleClient));
+                clientThread.Start(new Tuple<IPEndPoint, byte[]>(clientEndpoint, buffer));
+            }
+            else
+            {
+                Console.WriteLine("Máximo de clientes atingido");
+            }
+        }
+    }
+
+    
+    private static void InitializeDatabase()
+    {
+        // Certifica-se de ajustar o caminho do arquivo de banco de dados conforme necessário
+        string connectionString = "server=localhost;database=devstatus;port=3306;user=root;password=123456;";
+        MySqlConnection dbConnection = new MySqlConnection(connectionString);
+        
+
+try
+        {
+            // Abre a conexão com o banco de dados
+            dbConnection.Open();
+
+            // Cria a tabela 'dev_status' se não existir
+            using (MySqlCommand command = new MySqlCommand(
+                "CREATE TABLE IF NOT EXISTS dev_status (" +
+                "type INT, " +
+                "protocolo INT, " +
+                "utc DATETIME, " +
+                "status INT, " +
+                "id VARCHAR(255));", dbConnection))
+            {
+                command.ExecuteNonQuery();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Erro ao inicializar o banco de dados: " + ex.Message);
+        }
+        finally
+        {
+            // Certifique-se de fechar a conexão quando terminar
+            dbConnection.Close();
+        }
+    }
+
+    // Função que lida com os dados recebidos de um cliente
+    private static void HandleClient(object obj)
+    {
+        // Converte o objeto de parâmetros
+        Tuple<IPEndPoint, byte[]> clientData = (Tuple<IPEndPoint, byte[]>)obj;
+        IPEndPoint clientEndpoint = clientData.Item1;
+        byte[] buffer = clientData.Item2;//aqui to pegando o array de byte
+      
+        try
+        {
+
+            // Obtém o número de bytes recebidos
+            int receivedBytes = buffer.Length;
+
+            // Verifica se pelo menos um byte foi recebido
+            if (receivedBytes > 0)
+            {
+                // Converte os bytes para uma string usando UTF-8
+                string message = Encoding.UTF8.GetString(buffer, 0, receivedBytes);
+
+
+                string[] commandParts = message.Split(',');
+
+                int dataType = int.Parse(commandParts[0].Substring(5));//nessa parte eu estou pegando >data e so pegando o valor
+
+
+                int protocol = int.Parse(commandParts[1]);
+
+                string utc = commandParts[2];
+
+    
+                string[] finalCommand = commandParts[3].Split(';');
+                int status = int.Parse(finalCommand[0].Substring(0, 1));
+
+                string id = finalCommand[1].Substring(3, 3);//aqui eu to tirando os id< e to pegando os 3 digitos do id
+
+                // Armazenar os dados em um arquivo JSON
+
+                var armazenaDados = new ArmazenaDados
+                {
+                    type = dataType,
+                    protocolo = protocol,
+                    utc = DateTime.ParseExact(utc, "yyMMddHHmmss", null),
+                    status = status,
+                    id = id
+                };
+
+                string jsonString = JsonSerializer.Serialize(armazenaDados);
+                Console.WriteLine(jsonString);
+                File.AppendAllLines("./dev_status.json", new[] { jsonString });
+
+                InsertDataIntoDatabase(armazenaDados);
+            }
+        }
+        catch (Exception ex)
+        {
+            // Imprime mensagem de erro
+            Console.WriteLine("Erro ao processar mensagem: " + ex.Message);
+        }
+    }
+
+
+
+    // Função que insere dados na tabela 'dev_status' do banco de dados
+    private static void InsertDataIntoDatabase(ArmazenaDados armazenaDados)
+    {
+        
+        // Certifique-se de ajustar as informações de conexão conforme necessário
+        string connectionString = "server=localhost;database=devstatus;port=3306;user=root;password=123456;";
+        MySqlConnection dbConnection = new MySqlConnection(connectionString);
+
+        try
+        {
+            // Abre a conexão com o banco de dados
+            dbConnection.Open();
+
+            // Cria o comando SQL para inserção de dados
+            using (MySqlCommand command = new MySqlCommand(
+                "INSERT INTO dev_status (type, protocolo, utc, status, id) " +
+                "VALUES (@type, @protocolo, @utc, @status, @id);", dbConnection))
+            {
+                // Adiciona parâmetros ao comando
+                command.Parameters.AddWithValue("@Type",armazenaDados.type);
+                command.Parameters.AddWithValue("@protocolo",armazenaDados.protocolo);
+                command.Parameters.AddWithValue("@utc",armazenaDados.utc);
+                command.Parameters.AddWithValue("@status", armazenaDados.status);
+                command.Parameters.AddWithValue("@id", armazenaDados.id);
+
+                // Executa o comando de inserção
+                command.ExecuteNonQuery();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Erro ao inserir dados no banco de dados: " + ex.Message);
+        }
+        finally
+        {
+            // Certifique-se de fechar a conexão quando terminar
+            dbConnection.Close();
+        }
+    }
+}
+    public class ArmazenaDados
+{
+    public int type { get; set; }
+    public int protocolo { get; set; }
+    public DateTime utc { get; set; }
+    public int status { get; set; }
+    public string id { get; set; }
+}
+
